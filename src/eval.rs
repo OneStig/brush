927ed640e1bfb0b7586d2771e2526e05e7882d65
Suyash Mothukuri <s-smothukuri@lwsd.org>
2023-03-09 23:10:57 -0800

    Defining shape and circle implementations

--- src/eval.rs
+++ src/eval.rs
@@ -12,7 +12,51 @@ impl Interpreter {
         }
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         println!("Interpreting AST");
+    }
+
+}
+
+
+// defining functions for a Shape
+pub trait Shape {
+    fn set_radius(&mut self, radius : f64);
+    fn evolve(&mut self);
+    fn draw(&self, center: (f64, f64), generations: i32);
+}
+
+// defining Circle structure
+struct Circle {
+    radius: f64,
+}
+// defining keyword function specifically for circle
+impl Circle {
+    fn new(radius: f64) -> Circle {
+        Circle {
+            radius: radius,
+        }
+    }
+}
+
+// implementing circle as a shape that changes the radius based on functions accordingly 
+impl Shape for Circle {
+    fn set_radius(&mut self, radius: f64) {
+        self.radius = radius;
+    }
+
+    fn evolve(&mut self) {
+        self.radius /= 2.0;
+    }
+
+    fn draw(&self, center: (f64, f64), generations: i32) {
+        println!("Drawing circle with radius {} at center {:?} for {} generations", self.radius, center, generations);
+    }
+}
+
+// initializing a new circle into Shape with radius 
+impl Shape {
+    pub fn new_circle(radius: f64) -> impl Shape {
+        Circle::new(radius)
     }
 }

7449c8ab6673a8c86a06bb1998f8c98cbf3db928
s-mkrothapalli <71157973+s-mkrothapalli@users.noreply.github.com>
2023-03-24 00:18:31 -0700

    Update art.rs

--- src/art.rs
+++ src/art.rs
@@ -13,75 +13,25 @@ pub struct Shape {
     fill: (u8, u8, u8),
     outline_color: (u8, u8, u8),
     outline_width: f64,
-    rotation: f64,
+    rotation: f32,
     stretch: (f64, f64),
     warp: f64,
 }
 
-impl Shape {
-    pub fn new_circle(
-        center: (f64, f64),
-        radius: f64,
-    ) -> Shape {  
-        let mut path = Data::new();
-        let mut x = center.0 + radius;
-        let mut y = center.1;
-        let mut angle = 0.0;
 
-        while angle < 360.0 {
-            // path.move_to defines the starting point of the path
-            // path.line_to defines 
-            // path.move_to((x, y));
-            // let delta_x = radius * angle.cos();
-            // let delta_y = radius * angle.sin();
-            // path.line_to((center.0 + delta_x, center.1 + delta_y));
-            // x = center.0 + delta_x;
-            // y = center.1 + delta_y;
-            angle += 1.0;
-        }
-
-<<<<<<< Updated upstream
-        let svg = Path::new()
-            .set("fill", "none")
-            .set("stroke", "black")
-            .set("stroke-width", 1)
-            .set("d", path);
-        
-        // Shape {
-        //     svg: svg,
-        //     path: path,
-        //     center: center,
-        //     dimensions: (radius * 2.0, radius * 2.0),
-        // }
-        unimplemented!();
-=======
-        cdata = cdata.close();
-
-        Circle {
-            shape: Shape {
-                svg: Path::new()
-                    .set("fill", "none")
-                    .set("stroke", "black")
-                    .set("stroke-width", 1)
-                    .set("d", cdata.clone()),
-                path: cdata,
-                center: (x, y),
-                dimensions: (0.0, 0.0),
-                fill: (0, 0, 0),
-                outline_color: (0, 0, 0),
-                outline_width: 1.0,
-                rotation: 0.0,
-                stretch: (1.0, 1.0),
-            },
-            radius: radius,
-        }
-    }
+pub trait Drawable{
+    fn rotate(&mut self, angle: f32);
+    fn new_rect() -> Shape;
+    fn new_triangle() -> Shape;
+    fn new_polygon() -> Shape;
+    fn shift(&mut self, x: f64, y: f64);
 }
 
-impl Rectangle{
-    pub fn new(pos: (f32, f32), width: f32, height: f32) -> Rectangle {
+impl Shape {
+    pub fn new(pos: (f32, f32), width: f32, height: f32) -> Shape {
         let mut rdata = Data::new();
-
+        let x = pos.0;
+        let y = pos.1;
         rdata = rdata.move_to((x, y));
         rdata = rdata.line_to((x + width, y));
         rdata = rdata.line_to((x + width, y - height));
@@ -103,12 +53,73 @@ impl Rectangle{
                 outline_color: (0, 0, 0),
                 outline_width: 1.0,
                 stretch: (1.0, 1.0),
+                warp: 0.0,
+                center: ((pos.0 + (width/2.0)).into(), (pos.1-(height/2.0)).into()),
+                rotation: 0.0,
             },
             width: width,
             height: height,
         }
     }
+    pub fn new_circle(
+        center: (f64, f64),
+        radius: f64,
+    ) -> Shape {  
+        let mut cdata = Data::new();
+        let mut x = center.0 + radius;
+        let mut y = center.1;
+        let mut angle = 0.0;
+
+        while angle < 360.0 {
+            // path.move_to defines the starting point of the path
+            // path.line_to defines 
+            // path.move_to((x, y));
+            // let delta_x = radius * angle.cos();
+            // let delta_y = radius * angle.sin();
+            // path.line_to((center.0 + delta_x, center.1 + delta_y));
+            // x = center.0 + delta_x;
+            // y = center.1 + delta_y;
+            angle += 1.0;
+        }
+
+        let svg = Path::new()
+            .set("fill", "none")
+            .set("stroke", "black")
+            .set("stroke-width", 1)
+            .set("d", cdata);
+        
+        // Shape {
+        //     svg: svg,
+        //     path: path,
+        //     center: center,
+        //     dimensions: (radius * 2.0, radius * 2.0),
+        // }
+        unimplemented!();
+        cdata = cdata.close();
+
+        Circle {
+            shape: Shape {
+                svg: Path::new()
+                    .set("fill", "none")
+                    .set("stroke", "black")
+                    .set("stroke-width", 1)
+                    .set("d", cdata.clone()),
+                path: cdata,
+                center: (x, y),
+                dimensions: (0.0, 0.0),
+                fill: (0, 0, 0),
+                outline_color: (0, 0, 0),
+                outline_width: 1.0,
+                rotation: 0.0,
+                stretch: (1.0, 1.0),
+                warp: 0.0,
+            },
+            radius: radius,
+        }
+    }
 }
+
+//impl Rectangle{}
 
 
 impl Drawable for Shape {
@@ -170,32 +181,25 @@ impl Drawable for Shape {
                     .set("stroke", "black")
                     .set("stroke-width", 1)
                     .set("d", self.path.clone());
->>>>>>> Stashed changes
     }
 
-    pub fn new_rect() -> Shape {
+    fn new_rect() -> Shape {
         unimplemented!();
     }
 
-    pub fn new_triangle() -> Shape {
+    fn new_triangle() -> Shape {
         unimplemented!();
     }
 
-    pub fn new_polygon() -> Shape {
+    fn new_polygon() -> Shape {
         unimplemented!();
     }
 
-    pub fn shift(&mut self, x: f64, y: f64) {
+    fn shift(&mut self, x: f64, y: f64) {
         self.center.0 += x;
         self.center.1 += y;
 
         // iterate through the path and shift each point   
-    }
-
-    pub fn rotate(&mut self, angle: f64) {
-        self.rotation += angle;
-
-        // iterate through the path and rotate each point around the center
     }
 }
 
